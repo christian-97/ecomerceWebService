a9147de6a667448ccf3c69bd3484f9c20908e754
andyfgc <gonzalescolmenaresa@gmail.com>
2022-11-18 13:44:24 -0500

    holaasdasdasd

--- src/main/java/com/pe/proyecto/ProyectowebserviceApplication.java
+++ src/main/java/com/pe/proyecto/ProyectowebserviceApplication.java
@@ -3,6 +3,7 @@ package com.pe.proyecto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //un comnetario
+//hola
 @SpringBootApplication
 public class ProyectowebserviceApplication {
 
